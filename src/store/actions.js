fb97876e5d33ba9e90faa3508a7924ffdeba7e42
bluedistro <bineykingsley36@gmail.com>
2019-08-15 20:54:08 +0000

    merge conflict fix

--- src/store/actions.js
+++ src/store/actions.js
@@ -126,7 +126,6 @@ const getPricing = ({ commit }, requestPayload) => {
       destinations.push(dest_loc)
     }
 
-<<<<<<< HEAD
     var source;
     if (requestPayload.pickupData.searchAddress.source == "saved") {
       source =
@@ -140,11 +139,9 @@ const getPricing = ({ commit }, requestPayload) => {
         "," +
         requestPayload.pickupData.searchAddress.location.lng
     }
-=======
     const path = "https://api.mypasel.com/v2/route/get"
     // const path = "https://api.mypasel.com/v2/route/get?source=" + source + "&destinations=" + destinations + "&sourceID=" + requestPayload.pickupData.searchAddress.place_id
     var source = requestPayload.pickupData.searchAddress.location.lat + "," + requestPayload.pickupData.searchAddress.location.lng
->>>>>>> web-client-development
     const params = {
       source: source,
       destinations: destinations,
@@ -152,7 +149,6 @@ const getPricing = ({ commit }, requestPayload) => {
     }
     const path = "https://api.mypasel.com/v2/route/get"
     // const path = "https://api.mypasel.com/v2/route/get?source=" + source + "&destinations=" + destinations + "&sourceID=" + requestPayload.pickupData.searchAddress.place_id
-    alert('path ' + path)
     commit("request_payload_mutation", requestPayload)
     VueCookie.set(cookeys.REQUEST_DELIVERY_PAYLOAD_KEY, JSON.stringify(requestPayload), {
       expires: cookeys.cookie_expire
