50e9d7e5c33bfc8deb07c3ba832ab57ccdeb87eb
bluedistro <bineykingsley36@gmail.com>
2019-08-26 21:56:44 +0000

    mc resoln

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,6 +3,7 @@ import VueCookie from 'vue-cookie'
 import cookeys from '../cookeys'
 
 const loggedIn = (state) => {
+  // previously state.token as condition
   return state.token != null ? true : false
 }
 
